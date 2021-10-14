1bcf5f3fcebf67c0e8a0d32708bcae20860bdd28
thoma <thoma@DESKTOP-C8SAOAJ>
2021-10-14 10:59:12 +0200

    Phase 21 : sécurité

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -55,11 +55,14 @@ class VoyagesController extends AbstractController{
      * @return Response
      */
     public function findAllEqual($champ, Request $request) : Response {
-        $valeur = $request->get("recherche");
-        $visites = $this->repository->findByEqualValue($champ, $valeur);
-        return $this->render("pages/voyages.html.twig", [
-            'visites' => $visites
-        ]);
+        if($this->isCrsfTokenValid('filtre_'.$champ, $request->get('_token'))){
+            $valeur = $request->get("recherche");
+            $visites = $this->repository->findByEqualValue($champ, $valeur);
+            return $this->render("pages/voyages.html.twig", [
+                'visites' => $visites
+            ]);
+        }
+        return $this->redirectToRoute("voyages");       
     }
     
     /**
